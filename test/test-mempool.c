eb81127d9db3a775b82355871d738ea1cde64505
Jonas Claeson <jonas.cl@protonmail.com>
2019-04-14 10:24:45 +0200

    Change license to GPLv3

--- test/test-mempool.c
+++ test/test-mempool.c
@@ -1,8 +1,8 @@
 /*
- * 2019, Mokshasoft AB (mokshasoft.com)
+ * Copyright 2019, Mokshasoft AB (mokshasoft.com)
  *
  * This software may be distributed and modified according to the terms of
- * the Unlicense license. Note that NO WARRANTY is provided.
+ * the GNU General Public License version 3. Note that NO WARRANTY is provided.
  * See "LICENSE.txt" for details.
  */
 
